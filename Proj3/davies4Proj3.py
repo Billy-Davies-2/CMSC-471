f7eb8655f250978997592ea40b650faafd0ee0f4
Billy Davies <bdavies@Billys-MacBook-Air.local>
2016-04-27 14:50:37 -0400

    image conversion segment done

--- Proj3/davies4Proj3.py
+++ Proj3/davies4Proj3.py
@@ -1,3 +1,35 @@
-# Coded by: 	William Davies
-# Date:			04/24/2016
-# Class:		CMSC 471 - Project 3
+# Coded by:             William Davies
+# Date:                 04/24/2016
+# Class:                CMSC 471
+# Description:
+# This code will take an image file, and then convert it
+# to black and white. Then it will convert the black and
+# white image to a matrix representation.
+
+# imports
+from PIL import Image
+from numpy import *
+import sys
+from sklearn.neighbors import NearestNeighbors
+
+# convert an image to black and white.
+def toBlackWhite(filename):
+    image = Image.open(filename)
+    image = image.convert('1')
+    data = array(image)
+    #data = [image.getpixel((x, y)) for x in range(image.width) for y in range(image.height)]
+    bitmap = empty((data.shape[0],data.shape[1]),None)    #New array with same size as A
+
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j]==True:
+                bitmap[i][j]=0
+            else:
+                bitmap[i][j]=1
+    return bitmap
+
+def main():
+    filename = sys.argv[-1]
+    bitmapMatrix = toBlackWhite(filename)
+    print(bitmapMatrix)
+main()
