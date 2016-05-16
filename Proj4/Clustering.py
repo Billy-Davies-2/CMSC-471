3f3e03df781a2a3a5816966aa63455e7e797b0b1
Billy Davies <bdavies@Billys-MacBook-Air.local>
2016-05-16 13:30:17 -0400

    cleaned up some code

--- Proj4/Clustering.py
+++ Proj4/Clustering.py
@@ -9,7 +9,6 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-
 from sklearn.cluster import KMeans
 
 NUM_CLUSTERS = int(sys.argv[1])
@@ -19,12 +18,7 @@ myFile = open(FILE, 'r')
 X = np.loadtxt(FILE)
 plt.figure(figsize=(12, 12))
 
-#n_samples = 1500
-#random_state = 170
-#X, y = make_blobs(n_samples=n_samples, random_state=random_state)
-random_state = 20
-# Incorrect number of clusters
-y_pred = KMeans(n_clusters=NUM_CLUSTERS, random_state=random_state).fit_predict(X)
+y_pred = KMeans(n_clusters=NUM_CLUSTERS).fit_predict(X)
 
 plt.subplot(221)
 plt.scatter(X[:, 0], X[:, 1], c=y_pred)
